dfc727e7a69d052b59c39f4cc825e3ef30436c94
tanisman <mail@sezercantanisman.com.tr>
2017-12-23 01:07:41 +0200

    undefined to null

--- embedded17/controllers/tokenController.js
+++ embedded17/controllers/tokenController.js
@@ -33,8 +33,7 @@ module.exports = {
                 console.log("authorize token error: " + err);
                 callback(err, null);
             } else {
-                console.log(token);
-                if (token == undefined || token.access < required_access) {
+                if (token == null || token.access < required_access) {
                     callback(new Error('unauthorized action'), token);
                 } else {
                     callback(null, token);
