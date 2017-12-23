8df72ccf2bf6f22a0bd7dcde2c1eaa99f276af97
tanisman <mail@sezercantanisman.com.tr>
2017-12-23 02:04:08 +0200

    dummy

--- embedded17/routes/token.js
+++ embedded17/routes/token.js
@@ -1,8 +1,19 @@
 ﻿var express = require('express');
 var tokenController = require('../controllers/tokenController');
+var mongoose = require('mongoose'),
+    Token = mongoose.model('Tokens');
 
 var router = express.Router();
 
-router.post('/', tokenController.createToken);
-
+//router.post('/', tokenController.createToken);
+router.post('/', function (req, res) {
+    var new_token = new Token(req.body);
+    new_token.save(function (err, token) {
+        if (err) {
+            res.status(400).send({ success: 0, error: err.message });
+        } else {
+            res.json({ success: 1, created: token });
+        }
+    });
+});
 module.exports = router;
