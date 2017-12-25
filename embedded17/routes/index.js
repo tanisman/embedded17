b1013e0de70d373821be0fd8ecf054505e0c4b49
tanisman <mail@sezercantanisman.com.tr>
2017-12-25 22:08:33 +0200

    r e v e r s e

--- embedded17/routes/index.js
+++ embedded17/routes/index.js
@@ -17,7 +17,7 @@ router.get('/:my_token', function (req, res) {
                     res.status(400);
                     res.render('error', { message: err.message, error: {} });
                 } else {
-                    res.render('index', { title: 'Embedded17', results: log });
+                    res.render('index', { title: 'Embedded17', results: log.reverse() });
                 }
             }).populate('measure_unit', 'name');
         }
